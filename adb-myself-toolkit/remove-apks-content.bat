a3004a186a1f9cc3384551e27c6f59681bd76090
Syrone Wong <wong.syrone@gmail.com>
2015-01-07 20:11:29 +0800

    adb-myself-toolkit: misc
    check before remove apk files
    add newline

--- adb-myself-toolkit/remove-apks-content.bat
+++ adb-myself-toolkit/remove-apks-content.bat
@@ -4,7 +4,14 @@ echo    will remove all content in apks/ folder....
 echo -----------Press any key
 echo.
 pause & break
-
+if not exist apks\*.apk goto :bye
 del apks\*.apk /f /q 2>nul
+del apks\no_chinese_char /f /q 2>nul
 ::copy nul clean.tmp
 pause & break
+
+:bye
+cls
+echo  no apk files in apks/ folder
+pause & break
+exit
