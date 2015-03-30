108ec69b7ed15b51650c02a67bfed94ac95f3354
Syrone Wong <wong.syrone@gmail.com>
2015-03-30 10:24:01 +0800

    add ELAN touchpad control script

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 * install-wine-deps.sh  -->检测安装wine的安装依赖 已经支持 Ubuntu 14.04 LTS
 * terminalcolors.py  -->使用方法 python terminalcolors.py 检测终端（XTerm）的颜色支持，用于校验是否能够使用256色配色方案，如果能够输出各种颜色，证明功能正常
 * clean_compile_soft.py  --> 清理编译安装的软件，有些软件的 Makefile 中没有 uninstall 字段，只能先 make clean，然后 find 到所有相关文件之后再 rm，这个脚本很好用，通过虚拟安装到/tmp目录生成安装文件列表，然后清理，详细介绍在脚本中
+* touchpad-control.sh   --> 控制笔记本ELAN触控板开关
 * get-fonts.sh           --> 使用: chmod +x get-fonts.sh && sudo ./get-fonts.sh
 * bandwagonhost-debian-ss-polarssl.sh     --> Debian编译安装polarssl版本shadowsocks脚本  使用：wget --no-check-certificate https://raw.githubusercontent.com/wongsyrone/Scripts/master/bandwagonhost-debian-ss-polarssl.sh && chmod +x bandwagonhost-debian-ss-polarssl.sh && bash bandwagonhost-debian-ss-polarssl.sh
 * ip-dec2hex-netmask.sh   -->点分格式的ip地址列表转为HEX列表，可以生成 iptables 过滤规则，netmask版本
@@ -13,4 +14,4 @@
 * autoproxy20131215.xpi  --> 好吧，不是脚本，是Firefox修复版本（普通Logo）的Autoproxy.
 * fix-vac-tool/ 文件夹  --> 【CRLF】自己写的解决 Steam 平台提示计算机屏蔽 VAC 的解决工具
 * adb-myself-toolkit/ 文件夹  --> 【CRLF】自用的 ADB 工具
-* 
+
---
+++ touchpad-control.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+if ! which synclient > /dev/null ; then
+    echo "synclient not found. Plz reinstall touchpad driver."
+    exit 1
+fi
+
+status=`synclient -l | grep -i "touchpadoff" | sed -e "s/\s*//g" | awk -F"=" '{print $2}'`
+
+open() {
+    if [ "$status" = "0" ] ; then
+        echo "touchpad already open!"
+        exit 1
+    fi
+    synclient touchpadoff=0
+    RETVAL=$?
+    [ $RETVAL -eq 0 ] && echo "open success"
+    return $RETVAL
+}
+
+close() {
+    if [ "$status" = "1" ] ; then
+        echo "touchpad already closed!"
+        exit 1
+    fi
+    synclient touchpadoff=1
+    RETVAL=$?
+    [ $RETVAL -eq 0 ] && echo "close success"
+    return $RETVAL
+}
+
+switch() {
+if [ "$status" = "1" ] ; then
+    open
+else
+    close
+fi
+}
+
+case "$1" in
+open)
+open
+;;
+close)
+close
+;;
+switch)
+switch
+;;
+status)
+echo "Info: 0 stand for open"
+echo "      1 stand for closed"
+echo "Status is:    $status"
+;;
+*)
+echo $"Usage: $0 {open|close|switch|status}"
+RETVAL=1
+esac
+
+exit $RETVAL
+#synclient touchpadoff=0
